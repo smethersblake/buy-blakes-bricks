98d58223d29f6ea752ce2e181419416d66d58429
Blake Smethers <96027095+smethersblake@users.noreply.github.com>
2022-08-03 16:19:08 -0500

    find brick by part number

--- server/controllers/Brick-controller.js
+++ server/controllers/Brick-controller.js
@@ -10,6 +10,15 @@ const brickController = {
             console.log(err);
             res.status(400).json(err)
         })
+    },
+    getBrickByPartNumber (req, res)
+    {
+        Brick.find({part_num: req.params.part_num}).then(dbColorData => res.json(dbColorData))
+            .catch(err =>
+            {
+            console.log(err);
+            res.status(400).json(err)
+        })
     }
 }
 module.exports = brickController
--- server/routes/api/brick-routes.js
+++ server/routes/api/brick-routes.js
@@ -1,10 +1,14 @@
-const { getAllBrick } = require('../../controllers/Brick-controller')
+const { getAllBrick, getBrickByPartNumber } = require('../../controllers/Brick-controller')
 
 const router = require('express').Router()
 
 router
     .route('/')
     .get(getAllBrick)
+
+router
+    .route('/:part_num')
+    .get(getBrickByPartNumber)
 
 
 
