59010c1fbf630ae050b75d1f28e4fffc44b1e0be
Bob <rwillford0719@gmail.com>
2022-08-03 21:04:21 -0500

    added footer and about

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home'
+import About from './components/About'
 
 function App() {
   return (
+    <Router>
         <div>
           <Header />
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          
           <Footer />
         </div>
+    </Router>
   );
 }
 
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Heart from '../../pages/assets/LEGO_Heart 2.png'
 
 function Footer() {
     return (
-        <footer>
-            <h1 className="text-center text-2xl font-Play">Thanks for visting Blakes Bricks!</h1>
+        <footer className="p-4 bg-neutral-500 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            <span className="text-lg text-white sm:text-center dark:text-gray-400"><a href="/" className="hover:underline">BuyBlakesBricks </a>
+            Made with <img alt="Heart Shaped LEGO" width="50px" className="ml-20" src={Heart} /> Bricks 
+            </span>
+            
+            <ul className="flex flex-wrap items-center mt-3 text-lg text-white dark:text-gray-400 sm:mt-0">
+                <li className="hover:underline md:mr-6">
+                    <Link to="/about">About</Link>
+                </li>
+                <li className="hover:underline">
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
         </footer>
     )
 }
