6a5225fa3dcad5d8e0ee844b5397c2897a56ce7a
Christopher Langner <langnerc@icloud.com>
2022-08-05 06:39:42 -0500

    initial log in

--- client/src/App 2.js
+++ client/src/App 2.js
@@ -1,14 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import Login  from './pages/Login';
 
 function App() {
   return (
