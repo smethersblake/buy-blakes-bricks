5f8d063bfbaa6ba8b63e86e89b599b6c9ccd7f21
Bob <rwillford0719@gmail.com>
2022-08-05 15:48:18 -0500

    almost connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
@@ -5,19 +6,29 @@ import Footer from './components/Footer';
 import Home from './pages/Home'
 import About from './components/About'
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
 function App() {
   return (
-    <Router>
-        <div>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-          
-          <Footer />
-        </div>
-    </Router>
+    <ApolloClient client={client}>
+      <Router>
+          <div>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+            
+            <Footer />
+          </div>
+      </Router>
+    </ApolloClient>
   );
 }
 
