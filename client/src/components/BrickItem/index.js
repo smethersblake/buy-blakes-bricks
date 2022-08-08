331029b652ec70c8281ec4d9c96a6504e4f5609d
Bob <rwillford0719@gmail.com>
2022-08-08 18:51:12 -0500

    fixed css

--- client/src/components/BrickItem/index.js
+++ client/src/components/BrickItem/index.js
@@ -26,7 +26,7 @@ function BrickItem(item) {
             <Link to={`/bricks/${_id}`}>
                 <div className="flex flex-col items-center bg-white rounded-xl border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                     <img className="object-scale-down h-28 w-14 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={part_img_url} alt=""/>
-                    <div className=" p-4 leading-normal">
+                    <div className="p-4 leading-normal">
                         <h5 className="overflow-y-scroll h-24 ... mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Part Number: {part_num}</p>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Color: {color_name }</p>
